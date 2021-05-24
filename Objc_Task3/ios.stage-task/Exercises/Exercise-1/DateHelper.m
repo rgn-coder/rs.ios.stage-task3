311f6f2ba3ff56ba43afd66ad65e304d5c20347b
rgn-coder <reginadryagina@gmail.com>
2021-05-24 17:11:22 +0400

    feat: Task1 was completed

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,53 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    
+    NSDictionary *dictOfMonths = @{ @(1):@"January",
+                                    @(2):@"February",
+                                    @(3):@"March",
+                                    @(4):@"April",
+                                    @(5):@"May",
+                                    @(6):@"June",
+                                    @(7):@"July",
+                                    @(8):@"August",
+                                    @(9):@"September",
+                                    @(10):@"October",
+                                    @(11):@"November",
+                                    @(12):@"December"};
+    NSString *answer = dictOfMonths[@(monthNumber)];
+    return answer;
 }
+
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+    return [[NSCalendar currentCalendar] component:NSCalendarUnitDay fromDate:[dateFormatter dateFromString:date]];
 }
+
+
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    dateFormatter.dateFormat = @"EE";
+    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"];
+    NSLog(@"%@", [dateFormatter stringFromDate:date]);
+    return [dateFormatter stringFromDate:date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *currentDateComponents = [calendar components:(NSCalendarUnitWeekOfYear | NSCalendarUnitYear) fromDate:[NSDate now]];
+    NSDateComponents *givenDateComponents = [calendar components:(NSCalendarUnitWeekOfYear | NSCalendarUnitYear) fromDate:date];
+    
+    return (currentDateComponents.year == givenDateComponents.year) && (currentDateComponents.weekOfYear == givenDateComponents.weekOfYear);
 }
 
 @end
