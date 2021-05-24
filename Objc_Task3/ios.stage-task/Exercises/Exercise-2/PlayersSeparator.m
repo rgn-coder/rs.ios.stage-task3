b7439c1f2f1c78ed2847373602bc0878c8ea6374
rgn-coder <reginadryagina@gmail.com>
2021-05-24 21:41:09 +0400

    feat: Task2 was completed

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,22 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    if (!ratingArray || ratingArray.count < 3) {
+        return 0;
+    }
+    int answer = 0;
+    for (int i = 0; i < ratingArray.count - 2; i++) {
+        for (int j = i + 1; j < ratingArray.count - 1; j++) {
+            for (int k = j + 1; k < ratingArray.count; k++) {
+                if ((ratingArray[i].doubleValue < ratingArray[j].doubleValue) == (ratingArray[j].doubleValue < ratingArray[k].doubleValue)) {
+                    answer++;
+                }
+            }
+        }
+    }
+    return answer;
 }
+
+
 
 @end
