a07f6a86431a3f564410165685b154ae1d222047
rgn-coder <reginadryagina@gmail.com>
2021-05-24 22:17:30 +0400

    feat: Task3 was completed

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,38 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    
+    NSMutableString *result = [[NSMutableString alloc] init];
+    NSInteger left = 0;
+    NSInteger right = 0;
+    NSMutableString *leftChar;
+    NSMutableString *rightChar;
+    while (left < string1.length && right < string2.length) {
+        NSString *min;
+        if (leftChar == nil) {
+            leftChar = [NSMutableString stringWithString:[string1 substringWithRange:NSMakeRange(left, 1)]];
+        }
+        if (rightChar == nil) {
+            rightChar = [NSMutableString stringWithString:[string2 substringWithRange:NSMakeRange(right, 1)]];
+        }
+        if ([rightChar compare: leftChar] == NSOrderedAscending) {
+            min = rightChar;
+            rightChar = nil;
+            right++;
+        } else {
+            min = leftChar;
+            leftChar = nil;
+            left++;
+        }
+        [result appendString:min];
+    }
+    if (left < string1.length) {
+        [result appendString:[string1 substringFromIndex:left]];
+    } else {
+        [result appendString:[string2 substringFromIndex:right]];
+    }
+    
+    return result;
 }
 
 @end
